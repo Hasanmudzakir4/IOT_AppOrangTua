d6c0a262316c98ab3cb4075cc1d21ddafacd338e
Hasanmudzakir4 <hasanmudzakir0204@gmail.com>
2024-07-22 10:43:25 +0700

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.app.AppParent',
   appName: 'AppParent',
-  webDir: 'www'
+  webDir: 'www',
 };
 
 export default config;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiAnak:
+    'https://f32883b9-c54b-4fe3-858b-7223da13df40-00-1pefk9qri90it.picard.replit.dev/',
+  apiOrtu:
+    'https://0ee69490-d2cc-48c0-868f-33008a7a2aa9-00-d88r6911qph7.kirk.replit.dev/',
 };
 
 /*
